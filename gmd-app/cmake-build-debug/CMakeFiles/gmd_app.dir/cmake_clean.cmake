de47fc0b33feb94f8c34e00cb0aa3d352d086750
maifeeulasad <maifeeulasad@gmail.com>
2021-02-14 18:15:53 +0600

    //empty qt project

---
+++ gmd-app/cmake-build-debug/CMakeFiles/gmd_app.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gmd_app_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/gmd_app_autogen.dir/ParseCache.txt"
+  "gmd_app_autogen"
+  "CMakeFiles/gmd_app.dir/gmd_app_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/gmd_app.dir/main.cpp.o"
+  "gmd_app"
+  "gmd_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gmd_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gmd-app/cmake-build-debug/CMakeFiles/gmd_app_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gmd_app_autogen"
+  "gmd_app_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/gmd_app_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
