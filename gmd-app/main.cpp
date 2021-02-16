01be8e16061bdab663ebee2065f3e94fadb934cd
maifeeulasad <maifeeulasad@gmail.com>
2021-02-17 01:37:02 +0600

    ui hidden

--- gmd-app/main.cpp
+++ gmd-app/main.cpp
@@ -4,9 +4,6 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
 
     Manager manager(&a);
 
